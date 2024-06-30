6c1217e227f0edae74792bf65f60bea2fb9690d5
wirawancandra <wirawancandra631@gmail.com>
2024-06-30 20:50:05 +0700

    membuat fitur  login

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 const app=require("express");
 
 const server=app();
+server.use(app.json());
+server.use(app.urlencoded({extended:false}))
 server.get("/",(req,res)=>{
    
     res.send("Hello world");
+})
+server.post("/login",(req,res)=>{
+    const {username,password}=req.body;
+
+    if(username =="admin" && password =="admin"){
+        res.json({
+            status:200,
+            message:"Login successfully"
+        })
+    }
+    else {
+        res.json({
+            status:401,
+            message:"Login failed"
+        })
+    }
 })
 
 server.listen(3000,()=>console.log(`Running server in host http://localhost:3000`))
