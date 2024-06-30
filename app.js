9835f94c2e6d79fef20286014b648ca2865dfa93
wirawancandra <wirawancandra631@gmail.com>
2024-06-30 20:55:06 +0700

    membuat fitur logout

--- app.js
+++ app.js
@@ -5,5 +5,11 @@ server.get("/",(req,res)=>{
    
     res.send("Hello world");
 })
+server.post("/logout",(req,res)=>{
+    res.json({
+        status:200,
+        message:"logout success"
+    })
+})
 
 server.listen(3000,()=>console.log(`Running server in host http://localhost:3000`))
